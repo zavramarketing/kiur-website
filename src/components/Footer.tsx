c478157a43b27322cfe0efc7154c4fa347881ad4
emilia-kt <emilia@ettevotluskeskus.ee>
2026-06-09 23:48:17 +0300

    Increase logo size: h-14 in navbar, h-12 in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row items-center justify-between gap-6">
           <Link to="/" className="flex items-center">
-            <img src="/logo.png" alt="KIUR" className="h-8 w-auto object-contain brightness-0 invert" />
+            <img src="/logo.png" alt="KIUR" className="h-12 w-auto object-contain brightness-0 invert" />
           </Link>
 
           <div className="flex items-center gap-6">
